2a4242c5e1be2365d03baad204a6eada98af4e1c
HxTneshi <hinanayi.tenshi@gmail.com>
2016-01-30 03:02:21 +0900

    Revert "Revert "マージ""
    This reverts commit ff22c3a9a1c81d8f321ee059a8f09eb6f4043736.

---
+++ retual/Assets/kanta/SaraYogore.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaraYogore : MonoBehaviour {
+
+    
+
+    void OnTriggerBegin(Collider target)
+    {
+        if (target.tag == "Player")
+        {
+            Destroy(target);
+        }
+    }
+}
