4f98731f1abb7ea8f4b170c699a0edbc12200957
03hiyoko <m01133480e@edu.teu.ac.jp>
2016-01-30 08:56:55 +0900

    [ねもと]UIドラックドロップ途中まで

--- retual/Assets/nemoto/sprict/DragUI.cs
+++ retual/Assets/nemoto/sprict/DragUI.cs
@@ -3,27 +3,58 @@ using System.Collections;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-
+[RequireComponent(typeof (Image))]
 public class DragUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    public Transform canvasTran;
+    public  RectTransform obj;
+    public Vector3 startPosition;
+    public GameObject CopyObj;
+    public GameObject eobj;
+    public Image i;
 
-    public static RectTransform obj;
+    void Awake()
+    {
+        
+        canvasTran = transform.parent.parent;
+
+    }
 
     public void OnBeginDrag(PointerEventData e)
     {
-        obj = GetComponent<RectTransform>();
-        obj.SetAsFirstSibling();
+        CreateCopyObj();
+        CopyObj.transform.position = e.position;
+        
     }
 
     public void OnDrag(PointerEventData e)
     {
-        obj.position = e.position;
+        CopyObj.transform.position = e.position;
+       
     }
 
     public void OnEndDrag(PointerEventData e)
     {
-        obj.SetAsLastSibling();
+        gameObject.GetComponent<Image>().color = Vector4.one;
+        Destroy(CopyObj);
     }
 
+    
+    public void CreateCopyObj(){
+        CopyObj = new GameObject("CopyObj");
+        CopyObj.transform.SetParent(canvasTran);
+        CopyObj.transform.SetAsLastSibling();
+        CopyObj.transform.localScale = Vector3.one;
+
+        Image CopyImage = CopyObj.AddComponent<Image>();
+        Image sourceImage = transform.GetComponent<Image>();
+
+        CopyImage.sprite = sourceImage.sprite;
+        CopyImage.rectTransform.sizeDelta = sourceImage.rectTransform.sizeDelta;
+        CopyImage.color = sourceImage.color;
+        CopyImage.material = sourceImage.material;
+
+        gameObject.GetComponent<Image>().color = Vector4.one * 0.4f;
+    }
 	
 }
