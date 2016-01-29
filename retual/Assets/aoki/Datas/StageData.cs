725abc1dac165f2ccc23597afa01259507878b9b
S aoki <S aoki>
2016-01-30 06:55:11 +0900

    青木スクロールするUI追加　ステージデータ追加　モデル追加

---
+++ retual/Assets/aoki/Datas/StageData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class StageData  {
+
+    public int number;
+    public StageData( int number )
+    {
+        this.number = number;
+    }
+
+}
