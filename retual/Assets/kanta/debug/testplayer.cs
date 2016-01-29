90d07c8801836569458440a6524a24f00e7b9dae
HxTneshi <hinanayi.tenshi@gmail.com>
2016-01-30 04:18:07 +0900

    [藤平]皿洗い適当に実装

---
+++ retual/Assets/kanta/debug/testplayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testplayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float speed = 0.01f;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += transform.forward * -speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.right * speed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += transform.right * -speed;
+        }
+	}
+}
