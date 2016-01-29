1a58bc467629134110d600b8cbf8f8a0847a754b
03hiyoko <m01133480e@edu.teu.ac.jp>
2016-01-30 03:25:31 +0900

    [根本]UI画面だけ設定

---
+++ retual/Assets/nemoto/sprict/DropUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropUI : MonoBehaviour ,IDropHandler{
+
+    public void OnDrop(PointerEventData e)
+    {
+        DragUI.obj.position = transform.position;
+    }
+}
