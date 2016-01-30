4f679f64ce84a730a296dce0a833919ae3591bcd
HxTneshi <hinanayi.tenshi@gmail.com>
2016-01-30 20:51:09 +0900

    [藤平]リチャードの変更など

--- retual/Assets/nemoto/sprict/DragUI.cs
+++ retual/Assets/nemoto/sprict/DragUI.cs
@@ -45,7 +45,7 @@ public class DragUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
         CopyObj.transform.SetParent(canvasTran);
         CopyObj.transform.SetAsLastSibling();
         CopyObj.transform.localScale = Vector3.one;
-        CopyObj.transform.parent = GameObject.Find("Canvas").transform;
+        CopyObj.transform.parent = GameObject.Find("UniqueCanvas").transform;
 
         // レイキャストがブロックされないように
         CanvasGroup canvasGroup = CopyObj.AddComponent<CanvasGroup>();
