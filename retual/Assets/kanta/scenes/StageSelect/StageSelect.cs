eca0cca9b4bbe206b14b38cd46a5ded0b8b61b8a
HxTneshi <hinanayi.tenshi@gmail.com>
2016-01-30 16:05:42 +0900

    [藤平]ステージセレクトシーンの追加

---
+++ retual/Assets/kanta/scenes/StageSelect/StageSelect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StageSelect : MonoBehaviour {
+
+    [SerializeField]
+    int m_StageNo;
+
+
+    public void OnButton()
+    {
+        DataManager.Instance.stageData.number = m_StageNo;
+
+        if (SceneManager.GetSceneByName("GameScene").isLoaded == false)
+        {
+            SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+        }
+    }
+}
