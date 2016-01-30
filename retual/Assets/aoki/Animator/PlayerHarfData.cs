f4bc894567debbd5c964bd1b29c60433275e6d14
S aoki <S aoki>
2016-01-30 17:58:17 +0900

    Rechald

---
+++ retual/Assets/aoki/Animator/PlayerHarfData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class PlayerHarfData {
+
+    public int leg;
+    public int knee;
+    // 足首
+    public int ankle;
+
+    public int arm;
+    public int elbow;
+
+    public PlayerHarfData()
+    {
+        leg = 0;
+        knee = 0;
+        ankle = 0;
+        arm = 0;
+        elbow = 0;
+    }
+        
+
+}
