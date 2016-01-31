c93b330fb464d978fb2b4c69a02ea12fa293ec5b
03hiyoko <m01133480e@edu.teu.ac.jp>
2016-01-31 09:18:48 +0900

    [根本]BGMのスプリクトを一纏めにして途切れないように

---
+++ retual/Assets/nemoto/sprict/BGMMastor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMMastor : MonoBehaviour {
+
+    private static bool Created = false;
+
+    public AudioClip titleBGM;
+    public AudioClip MainBGM;
+    public string MainSceneName;
+    public bool MainSceneChack;
+
+    void Awake()
+    {
+        if(!Created){
+        DontDestroyOnLoad(transform.gameObject);
+        Created =true;
+    }else{
+        Destroy(transform.gameObject);
+    }
+    }
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!MainSceneChack)
+        {
+            if (Application.loadedLevelName == MainSceneName)
+            {
+                transform.GetComponent<AudioSource>().clip = MainBGM;
+                transform.GetComponent<AudioSource>().Play();
+                MainSceneChack = true;
+            }
+        }
+        else
+        {
+             if (Application.loadedLevelName != MainSceneName)
+            {
+                transform.GetComponent<AudioSource>().clip = titleBGM;
+                transform.GetComponent<AudioSource>().Play();
+                MainSceneChack=false;
+            }
+        }
+	
+	}
+}
